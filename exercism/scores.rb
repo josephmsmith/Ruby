b377f72504da9dc7588c6f4b60dfec5f9c9a2534
Joseph Smith <joseph.smit95@gmail.com>
2024-05-28 16:57:42 -0400

    refactored using attr_reader

--- exercism/scores.rb
+++ exercism/scores.rb
@@ -1,29 +1,51 @@
+
+# refactored code using attr_reader
 class HighScores
+    attr_reader :scores
     def initialize(scores)
       @scores = scores
     end
-  
-    def scores
-      @scores
-    end
-
     def latest
-        @scores.last
+      scores.last
     end
-
     def personal_best
-        @scores.max
+      scores.max
     end
-
     def personal_top_three
-        # refactored
-        # @scores.sort.slice(-3..).reverse
-        @scores.uniq.max(3)
+      scores.uniq.max(3)
     end
-
     def latest_is_personal_best?
-        @scores.last == @scores.max
+      latest == personal_best 
     end
-
-  end
+end
   
+# original submission
+
+# class HighScores
+#     def initialize(scores)
+#       @scores = scores
+#     end
+  
+#     def scores
+#       @scores
+#     end
+
+#     def latest
+#         @scores.last
+#     end
+
+#     def personal_best
+#         @scores.max
+#     end
+
+#     def personal_top_three
+#         # refactored
+#         # @scores.sort.slice(-3..).reverse
+#         @scores.uniq.max(3)
+#     end
+
+#     def latest_is_personal_best?
+#         @scores.last == @scores.max
+#     end
+# end
+
